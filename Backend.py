344f11a87702323e891954790b488de454e44e17
Lucas José Povinske <lucasjp96@gmail.com>
2021-09-16 22:06:39 -0300

    Adição de id e GETs

--- Backend.py
+++ Backend.py
@@ -2,10 +2,12 @@
 import mariadb
 import json
 from dataclasses import dataclass
+import dataclasses
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import select, create_engine
+from sqlalchemy.orm import sessionmaker
 import sys
 
 app = Flask(__name__)
@@ -13,6 +15,13 @@ CORS(app)
 path = "mysql+pymysql://root:root@127.0.0.1:3306/PACER"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = path
+
+'''
+#Abrir Sessão:
+engine = create_engine(path)
+Session = sessionmaker(engine)
+session = Session()
+'''
 
 db = SQLAlchemy(app)
 
@@ -42,6 +51,9 @@ class Avaliacao(db.Model):
     ava_avaliador = db.Column(db.BigInteger)
     ava_projeto = db.Column(db.BigInteger)
 
+    def as_dict(self):
+       return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
 @dataclass
 class Usuario(db.Model):
     usu_id: int
@@ -57,6 +69,9 @@ class Projeto(db.Model):
     
     pro_id = db.Column(db.BigInteger, primary_key=True)
     pro_equipe = db.Column(db.BigInteger)
+
+    def as_dict(self):
+       return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
 
 @dataclass
 class Projeto_Equipe(db.Model):
@@ -94,8 +109,10 @@ class Aluno(db.Model):
     alu_ra = db.Column(db.BigInteger)
     alu_usuario = db.Column(db.BigInteger)
 
+    def as_dict(self):
+       return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
+
 def buscar_alunos(proj):
-    
     equ = []
     alu = []
     a = []
@@ -111,8 +128,18 @@ def buscar_alunos(proj):
         a.append(alu)
     return a
 
-@app.route('/nota', methods=['POST', 'GET'])
-def inserir_notas():
+'''
+def buscar_ava():
+    proj = Projeto.query.all()
+    usuario = '7'
+    for p in proj:
+        j = p.as_dict()
+        j.pop('pro_equipe')
+        print(j)
+'''
+        
+@app.route('/aluno/<string:id>/nota', methods=['POST', 'GET'])
+def inserir_notas(id):
 
     # POST das notas:
     if request.method == 'POST':
@@ -133,11 +160,14 @@ def inserir_notas():
             
     # GET das avaliações:
     if request.method == 'GET':
-        avaliacoes = Avaliacao.query.all()
+        avaliacoes = []
+        ava = Avaliacao.query.filter(Avaliacao.ava_avaliador == id).all()
+        for a in ava:
+            avaliacoes.append(a.as_dict())
         return jsonify(avaliacoes)
 
-@app.route('/professor/avaliacao', methods=['POST', 'GET'])
-def avaliacao():
+@app.route('/professor/<string:id>/avaliacao', methods=['POST', 'GET'])
+def avaliacao(id):
 
     # POST da avaliacao:
     if request.method == 'POST':
@@ -178,12 +208,16 @@ def avaliacao():
                         print(e)
             
     # GET dos membros da equipe:
-    #if request.method == 'GET':
-        
-    
-
+    if request.method == 'GET':
+        projetos = []
+        proj = Projeto.query.all()
+        for pr in proj:
+            p = pr.as_dict()
+            p.pop('pro_equipe')
+            projetos.append(p)
+        return jsonify(projetos)
 
 if __name__ == '__main__':
-
+    
     app.debug = True
     app.run()
