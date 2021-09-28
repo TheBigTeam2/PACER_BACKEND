48cf02d23930be7b91d0d19e03fe208b327b7ef1
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2021-09-27 22:09:37 -0300

    Refatorando para redução de repetição de código

--- dao/BaseDao.py
+++ dao/BaseDao.py
@@ -16,3 +16,24 @@ class BaseDao(ABC):
             print(error)
             return False
     
+
+    def save_entity_with_commit(self, object) -> bool:
+        is_saved = self.save(object)
+
+        if is_saved:
+            self.session.commit()
+        else:
+            self.session.rollback()
+
+        return is_saved
+
+    def save_entity_in_mass(self, collection: list) -> bool:
+        db_insertions = [self.save_entity_with_commit(item) for item in collection]
+
+        if all(is_saved for is_saved in db_insertions):
+            self.session.commit()
+            return True
+        
+        else:
+            self.session.rollback()
+            return False
--- dao/NotaDao.py
+++ dao/NotaDao.py
@@ -14,25 +14,8 @@ class NotaDao(BaseDao):
             not_valor = json["nota"]
         )
 
-
-    def save_nota(self,object: Nota) -> bool:
-
-        is_saved = self.save(object)
-        if is_saved:
-            self.session.commit()
-        else:
-            self.session.rollback()
-
-        return is_saved
-
     def save_notas_in_mass(self, notas_collection: list):
 
-        db_insertions = [self.save(self.create_nota(nota)) for nota in notas_collection]
+        entitys = [self.create_nota(nota) for nota in notas_collection]
 
-        if all(is_saved for is_saved in db_insertions):
-            self.session.commit()
-            return True
-        
-        else:
-            self.session.rollback()
-            return False
+        return self.save_entity_in_mass(entitys)
