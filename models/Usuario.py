83b29a66dfb3a2ba0d9372c94b8cbf90d972a64e
Lucas José Povinske <lucasjp96@gmail.com>
2021-09-30 21:28:41 -0300

    Update Usuario

--- models/Usuario.py
+++ models/Usuario.py
@@ -5,15 +5,15 @@ from dataclasses import dataclass
 class Usuario(Base):
     usu_id: int
     usu_nome: str
-    usu_email: str
-    usu_senha: str
+    #usu_email: str
+    #usu_senha: str
     
     __tablename__ = "usuario"
 
     usu_id = Column(Integer,primary_key=True)
     usu_nome = Column(String)    
-    usu_email = Column(String)
-    usu_senha = Column(String)
+    #usu_email = Column(String)
+    #usu_senha = Column(String)
 
     def as_dict(self):
        return {c.name: str(getattr(self, c.name)) for c in self.__table__.columns}
